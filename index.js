44912045f6305f949aba3ed5cc2e849cc614b991
sajjadahammed <sajjad08@protonmail.com>
2022-12-11 16:19:05 +0530

    Frontend Done

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require('path')
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
 
@@ -8,6 +9,9 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+//Set static folder
+app.use(express.static(path.join(__dirname,'public')))
 
 app.use("/openai", require("./routes/openaiRoutes"));
 
